10bed2730194d6674bbd44b4a201d2aa28ab185e
Joseph DeCrisanti <joseph_decrisanti@icloud.com>
2018-10-18 18:25:35 -0400

    Random B
    Figured out how to get the numbers to gererate without repeats

--- MobileAppDevFinal/BingoCardViewController.swift
+++ MobileAppDevFinal/BingoCardViewController.swift
@@ -10,21 +10,37 @@ import UIKit
 
 class BingoCardViewController: UIViewController {
 
+    @IBOutlet weak var B1Button: UIButton!
+    @IBOutlet weak var B2Button: UIButton!
+    @IBOutlet weak var B3Button: UIButton!
+    @IBOutlet weak var B4Button: UIButton!
+    @IBOutlet weak var B5Button: UIButton!
+    
+    
+    
+    var ButtonRow : [UIButton] = [UIButton]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        numberGenerator()
+        
+      
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func numberGenerator(){
+        self.ButtonRow = [self.B1Button, self.B2Button, self.B3Button, self.B4Button, self.B5Button]
+        
+        var bNumbers = [1,2,3,4,5,6,7,8,9,10,12,13,14,15]
+        
+        for button in ButtonRow{
+            
+            let pickedNumber = Int.random(in: 0...bNumbers.count)
+            let number = bNumbers[pickedNumber]
+            bNumbers.remove(at: pickedNumber)
+            
+            button.setTitle("\(number)", for: .normal)
+        }
     }
-    */
-
 }
